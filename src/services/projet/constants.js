81bac29cf0704514946a1a10c853b7475410870d
Alexiafe <34021784+Alexiafe@users.noreply.github.com>
2017-12-22 19:45:36 +0100

    Update constants.js

--- src/services/projet/constants.js
+++ src/services/projet/constants.js
@@ -1,4 +1,5 @@
 
+
 export const AJOUTE_STOCK = "@stock - add product";
 export const SUPPRIME_STOCK = "@stock - delete product";
 export const INCREMENT_STOCK = "@stock - +1";
@@ -10,3 +11,5 @@ export const VALIDE_COMMANDE = "@panier - valideCommande";
 export const VALIDE_EDIT = "@stock - valideEdit";
 export const SET_PANIER_1 = "@achats - ->> true";
 export const SET_PANIER_2 = "@achats - ->> false";
+export const INCREMENT_STOCK_2 = "@stock - +1/-1";
+export const DECREMENT_STOCK_2 = "@stock - -1/+1";
